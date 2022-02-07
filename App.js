56866cfac1d6a448a10e0b2dcbe159df20adc7b7
Vetal339 <vitalijkadylinskij@gmail.com>
2022-02-07 16:20:39 +0300

    create react-native app and  settings for android/ use first classic components

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, Button, Alert, Image } from 'react-native';
 
 export default function App() {
+    const handleTextPress = () => console.log('presss')
+    const handleButtonPress = () => Alert.alert("Native", "Main message", [
+        {text: "yes", onPress: () => console.log('yes button')},
+        {text: 'cansel', onPress: () => console.log('cansel button')}
+    ])
+    const handleButtonPress2 = () => Alert.prompt("Native", "Main message",text => console.log(text));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} style={styles.text} onPress={handleTextPress} >Hello! {'\n'}brother</Text>
+        <Button title={'click me'} onPress={handleButtonPress} />
+        <Button title={'click2 me'} onPress={handleButtonPress2}/>
+
+        <Image source={require('./assets/adaptive-icon.png')}/>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      marginTop: 50,
+      flex: 1,
+      backgroundColor: '#fff',
+      justifyContent: 'flex-start',
   },
+    text: {
+      alignSelf: 'center',
+      color: 'red',
+    },
 });
